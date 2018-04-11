c956917d4050e5cba866cdbe3296e5ca160e2ea4
dan kahle <dan.kahle@gmail.com>
2018-04-10 17:18:26 -0700

    ui added and app comp

--- src/app/shared/directives/make-red.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import {MakeRedDirective} from './make-red.directive';
-
-describe('MakeRedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MakeRedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/lala.ts
+++
@@ -1,5 +0,0 @@
-
-
-class Test {
-  stuff = 'lala';
-}
